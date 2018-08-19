fda98d820186cace5487df6d4bcdd3a74e92c282
ranj13 <42338027+ranj13@users.noreply.github.com>
2018-08-19 02:12:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>master branch</title>
+        <title>student branch</title>
+        <title>student branch</title>
     </head>
     <body>
         <?php
